cfec2052c1d07088c1d93222a106e32214e6f196
Brianna Renni <briannaxrene@icloud.com>
2023-01-17 16:36:07 -0800

    Delete menu component

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeComponent} from './home/home.component';
-import {MenuPageComponent} from './menu-page/menu-page.component';
 
 @NgModule({
 	imports: [
 		CommonModule,
 	],
-	declarations: [HomeComponent, MenuPageComponent],
-	exports: [HomeComponent, MenuPageComponent]
+	declarations: [HomeComponent],
+	exports: [HomeComponent]
 })
 
 export class ComponentsModule { }
