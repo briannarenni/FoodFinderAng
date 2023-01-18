827fe30891f23e4850e65179a9fb616448cde1e2
Brianna Renni <briannaxrene@icloud.com>
2023-01-18 12:48:54 -0800

    Dropdown array, menu modal fix

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -6,12 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  cuisines!: ['American', 'Chinese', 'Greek', 'Italian', 'Mexican', 'Thai'];
-  cities!: ['Atlanta', 'Chicago', 'Houston', 'Los Angeles', 'Miami', 'New Orleans', 'New York City', 'Orlando', 'Portland', 'Seattle', 'San Diego', 'San Francisco'];
+  cuisines!: String[];
+  cities!: String[];
 
   constructor() { }
 
   ngOnInit() {
+    this.cuisines = ['American', 'Chinese', 'Greek', 'Italian', 'Mexican', 'Thai'];
+    this.cities = ['Atlanta', 'Chicago', 'Houston', 'Los Angeles', 'Miami', 'New Orleans', 'New York City', 'Orlando', 'Portland', 'Seattle', 'San Diego', 'San Francisco'];
   }
 
 }
